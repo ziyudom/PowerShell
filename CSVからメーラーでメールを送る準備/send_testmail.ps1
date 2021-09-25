c4230d9bee796b3d9c314d069640afdebc61ad4b
ziyudom <tokushima@ziyudom.com>
2021-09-25 17:20:10 +0900

    どんどん
    本文テキスト外に出して一か所差し替え可能にした

--- CSVからメーラーでメールを送る準備/send_testmail.ps1
+++ CSVからメーラーでメールを送る準備/send_testmail.ps1
@@ -1,44 +1,57 @@
 ﻿
 #タイトルとアドレスからメールを生成する
+$BR = "%0D%0A" #改行コード
+#現在ディレクトリ取得
+$CurrentDir = Split-Path $MyInvocation.MyCommand.Path
 
-function MAKE_MAIL($name,$address) {
+#メールbody用のテキストファイル取得
+$txtFilePath = ($CurrentDir)+"\BODYテンプレート.txt"
+#送信リストのCSVファイルパス
+$filePath = ($CurrentDir)+"\サンプル.csv"
+
+function MAKE_MAIL($title,$address,$bodyTxt) {
  
-    # 変数定義
-    $Subject = "テストメール" + ($name)
+    $Subject = $title
     $MailTo = $address
-    $Body = @()
-    $CrLf = "`r`n"
-
-    echo $Subject
-
-    $MailBody = "これはテストです"
+    $MailBody = $bodyTxt
 
     # mailto のリンク形式の文字列を生成
     $EncodeTxt = 'mailto:' + $MailTo + `
                     '?subject=' + ($Subject) + `
-                    '&body=' + ($MailBody) 
+                    '&body=' + ($MailBody)
                 
     Start-Process $EncodeTxt
+}
 
+#テキストファイルを読み込む
+function READ_TEXT($txtFilePath){
 
+    $f = (Get-Content -Encoding UTF8 $txtFilePath) -as [string[]]
+
+    #返却用テキスト
+    $rtnText = ""
+    #一行ごとに改行コードつける
+    foreach ($l in $f) {
+      $rtnText = $rtnText + $l + $BR
+    }
+    
+    return $rtnText
 }
  
-#現在ディレクトリ取得
-$CurrentDir = Split-Path $MyInvocation.MyCommand.Path
-echo $CurrentDir
+#テキストファイルから読み込む
+$tempBodyText = READ_TEXT $txtFilePath
 
-#CSVファイルパス
-$filePath = ($CurrentDir)+"\サンプル.csv"
-echo $filePath
-
-#CSV読み込み
+#CSV読み込み(UTF8) ※EXCELで作成してメモ帳で名前を付けて保存からUTF8に変換する
 $products = Import-Csv $filePath  -Encoding UTF8
 $products | Format-Table
 
-#ぐるぐる回す
+#CSV分ぐるぐる回す
 Import-Csv $filePath | ForEach-Object {
 
-  MAKE_MAIL $_.number $_.address
+  #テキスト差し替え
+  $bodyText = $tempBodyText.Replace("{{text}}",$_.text)
+  #メールを作成する
+  MAKE_MAIL $_.title $_.address $bodyText
 
 }
 
